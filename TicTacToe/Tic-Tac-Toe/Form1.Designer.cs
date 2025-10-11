69b1975bda1dc7592cfbbc4c72173591815b98f3
MohamedMostafa02 <mohamedmostafa.personal@gmail.com>
2025-10-11 18:40:57 +0300

    Add TicTacToe app

---
+++ TicTacToe/Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbTurn = new System.Windows.Forms.Label();
+            this.lbPlayer1 = new System.Windows.Forms.Label();
+            this.lbWinner = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lbInProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button1.Location = new System.Drawing.Point(588, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 157);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button2.Location = new System.Drawing.Point(868, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 157);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button3.Location = new System.Drawing.Point(1148, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 157);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button4.Location = new System.Drawing.Point(588, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 157);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "?";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button5.Location = new System.Drawing.Point(868, 386);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 157);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "?";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button6.Location = new System.Drawing.Point(1148, 382);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 157);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "?";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button7.Location = new System.Drawing.Point(588, 587);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 157);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "?";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button8.Location = new System.Drawing.Point(868, 587);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(170, 157);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "?";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Image = global::Tic_Tac_Toe.Properties.Resources.question_mark_96;
+            this.button9.Location = new System.Drawing.Point(1148, 579);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(170, 157);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "?";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.White;
+            this.lbTitle.Location = new System.Drawing.Point(692, 19);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(548, 69);
+            this.lbTitle.TabIndex = 9;
+            this.lbTitle.Text = "Tic-Tac-Toe Game";
+            // 
+            // lbTurn
+            // 
+            this.lbTurn.AutoSize = true;
+            this.lbTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTurn.ForeColor = System.Drawing.Color.Yellow;
+            this.lbTurn.Location = new System.Drawing.Point(189, 239);
+            this.lbTurn.Name = "lbTurn";
+            this.lbTurn.Size = new System.Drawing.Size(157, 69);
+            this.lbTurn.TabIndex = 10;
+            this.lbTurn.Text = "Turn";
+            // 
+            // lbPlayer1
+            // 
+            this.lbPlayer1.AutoSize = true;
+            this.lbPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPlayer1.ForeColor = System.Drawing.Color.White;
+            this.lbPlayer1.Location = new System.Drawing.Point(130, 331);
+            this.lbPlayer1.Name = "lbPlayer1";
+            this.lbPlayer1.Size = new System.Drawing.Size(337, 91);
+            this.lbPlayer1.TabIndex = 11;
+            this.lbPlayer1.Text = "Player 1";
+            // 
+            // lbWinner
+            // 
+            this.lbWinner.AutoSize = true;
+            this.lbWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWinner.ForeColor = System.Drawing.Color.Yellow;
+            this.lbWinner.Location = new System.Drawing.Point(161, 439);
+            this.lbWinner.Name = "lbWinner";
+            this.lbWinner.Size = new System.Drawing.Size(225, 69);
+            this.lbWinner.TabIndex = 12;
+            this.lbWinner.Text = "Winner";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Black;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.White;
+            this.btnRestart.Location = new System.Drawing.Point(146, 621);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(333, 67);
+            this.btnRestart.TabIndex = 13;
+            this.btnRestart.Text = "Restart Game";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // lbInProgress
+            // 
+            this.lbInProgress.AutoSize = true;
+            this.lbInProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInProgress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbInProgress.Location = new System.Drawing.Point(134, 532);
+            this.lbInProgress.Name = "lbInProgress";
+            this.lbInProgress.Size = new System.Drawing.Size(346, 69);
+            this.lbInProgress.TabIndex = 14;
+            this.lbInProgress.Text = "In Progress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1840, 870);
+            this.Controls.Add(this.lbInProgress);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lbWinner);
+            this.Controls.Add(this.lbPlayer1);
+            this.Controls.Add(this.lbTurn);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lbTurn;
+        private System.Windows.Forms.Label lbPlayer1;
+        private System.Windows.Forms.Label lbWinner;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lbInProgress;
+    }
+}
+
---
+++ TicTacToe/Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tic_Tac_Toe.Properties;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        stGameStatus GameStatus;
+        enPlayer PlayerTurn = enPlayer.Player1;
+        
+
+        enum enPlayer
+        {
+            Player1,
+            player2
+        }
+
+        enum enWinner
+        {
+            Player1,
+            player2,
+            Draw,
+            GameInProgress
+        }
+
+        struct stGameStatus
+        {
+            public enWinner Winner;
+            public bool GameOver;
+            public short PlayCount;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool CheckValues(Button btn1,Button btn2, Button btn3)
+        {
+            if (btn1.Tag.ToString() != "?" && btn1.Tag.ToString() == btn2.Tag.ToString() && btn1.Tag.ToString() == btn3.Tag.ToString())
+            {
+                btn1.BackColor = Color.GreenYellow;
+                btn2.BackColor = Color.GreenYellow;
+                btn3.BackColor = Color.GreenYellow;
+                 
+
+
+                if (btn1.Tag.ToString() == "X")
+                {
+                    GameStatus.Winner = enWinner.Player1;
+                    GameStatus.GameOver = true;
+                    EndGame();
+                    return true;
+                }
+                else
+                {
+                    GameStatus.Winner = enWinner.player2;
+                    GameStatus.GameOver = true;
+                    EndGame();
+                    return true;
+                }
+
+
+            }
+
+            GameStatus.GameOver = false;
+            return false;
+        }
+
+        void EndGame()
+        {
+            lbTurn.Text = "Game Over";
+            switch (GameStatus.Winner)
+            {
+                case enWinner.Player1:
+                    lbWinner.Text = "Player1";
+                    break;
+                case enWinner.player2:
+                    lbWinner.Text = "Player2";
+                    break;
+                default:
+                    lbWinner.Text = "Draw";
+                    break;
+            }
+
+            MessageBox.Show("GameOver", "GameOver", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void CheckWinner()
+        {
+            //Check rows
+            //Check row1
+            if (CheckValues(button1, button2, button3))
+                return;
+            //check row2
+            if (CheckValues(button4, button5, button6))
+                return;
+            //check row3
+            if (CheckValues(button7, button8, button9))
+                return;
+
+            //check cols
+            //check col1
+            if (CheckValues(button1, button4, button7))
+                return;
+            //check col2
+            if (CheckValues(button2, button5, button8))
+                return;
+            //check col3
+            if (CheckValues(button3, button6, button9))
+                return;
+
+            //check diagonal
+
+            //check diagonal1
+            if (CheckValues(button1, button5, button9))
+                return;
+
+            //check diagonal2
+            if (CheckValues(button3, button5, button7))
+                return;
+
+        }
+
+        public void ChangeImage(Button btn)
+        {
+            if(btn.Tag.ToString() == "?")
+            {
+                switch (PlayerTurn)
+                {
+                    case enPlayer.Player1:
+                        btn.Image = Resources.X;
+                        PlayerTurn = enPlayer.player2;
+                        lbTurn.Text = "Player 2";
+                        GameStatus.PlayCount++;
+                        btn.Tag = "X";
+                        CheckWinner();
+                        break;
+                    case enPlayer.player2:
+                        btn.Image = Resources.O;
+                        PlayerTurn = enPlayer.Player1;
+                        lbTurn.Text = "Player 1";
+                        GameStatus.PlayCount++;
+                        btn.Tag = "O";
+                        CheckWinner();
+                        break;
+                    
+                }
+             
+            }
+            else
+            {
+              MessageBox.Show("Wrong Choice", "Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if(GameStatus.PlayCount == 9 && !GameStatus.GameOver)
+            {
+                GameStatus.GameOver = true;
+                GameStatus.Winner = enWinner.Draw;
+                EndGame();
+            }
+
+        }
+
+
+
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color white = Color.FromArgb(255, 255, 255, 255);
+            Pen whitePen = new Pen(white);
+            whitePen.Width = 15;
+
+            whitePen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            whitePen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            //draw horizental lines
+            e.Graphics.DrawLine(whitePen, 400, 300, 1050, 300);
+            e.Graphics.DrawLine(whitePen, 400, 460, 1050, 460);
+
+            //draw vertical lines
+
+            e.Graphics.DrawLine(whitePen, 610, 140, 610, 620);
+            e.Graphics.DrawLine(whitePen, 840, 140, 840, 620);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            ChangeImage((Button) sender);
+        }
+
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button1);
+        //}
+
+        //private void button8_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button8);
+        //}
+
+        //private void button5_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button5);
+        //}
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button2);
+        //}
+
+        //private void button4_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button4);
+        //}
+
+        //private void button3_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button3);
+        //}
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button6);
+        //}
+
+        //private void button9_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button9);
+        //}
+
+        //private void button7_Click(object sender, EventArgs e)
+        //{
+        //    ChangeImage(button7);
+        //}
+
+        public void RestButton(Button btn)
+        {
+            btn.Image = Resources.question_mark_96;
+            btn.Tag = "?";
+            btn.BackColor = Color.Transparent;
+        }
+
+        private void RestartGame()
+        {
+            RestButton(button1);
+            RestButton(button2);
+            RestButton(button3);
+            RestButton(button4);
+            RestButton(button5);
+            RestButton(button6);
+            RestButton(button7);
+            RestButton(button8);
+            RestButton(button9);
+            PlayerTurn = enPlayer.Player1;
+            lbTurn.Text = "Player 1";
+            GameStatus.PlayCount = 0;
+            GameStatus.GameOver = false;
+            GameStatus.Winner = enWinner.GameInProgress;
+            lbWinner.Text = "In Progress";
+
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();  
+        }
+    }
+}
